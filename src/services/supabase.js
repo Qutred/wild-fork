38627b18dc83e80f994c2cbe3861b0ca7386d5c5
Qutred <shloser.andriy@gmail.com>
2024-02-28 15:57:44 +0200

    added supabase lib and base connection

---
+++ src/services/supabase.js
@@ -0,0 +1,10 @@
+import { createClient } from '@supabase/supabase-js';
+const supabaseUrl = 'https://xhlssdfcbxlnzigrrsnz.supabase.co';
+//process.env.SUPABASE_KEY;
+const supabaseKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhobHNzZGZjYnhsbnppZ3Jyc256Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDkwNDk3MDIsImV4cCI6MjAyNDYyNTcwMn0.e9I-ivN7Is5eYdTmWlSQcyE6ZxzjbRGaBbcE383-YuE';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+console.log(supabase);
+
+export default supabase;
