7e11292b38e58a73d39d8b7ed2f66a6ce78df41e
Qutred <shloser.andriy@gmail.com>
2024-03-18 21:31:15 +0200

    added feature for updating user data

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,4 +1,6 @@
 import supabase from './supabase';
+import { supabaseUrl } from './supabase';
+import { nanoid } from 'nanoid';
 
 export const signup = async ({ fullName, email, password }) => {
   let { data, error } = await supabase.auth.signUp({
@@ -43,4 +45,36 @@ export const getCurrentUser = async () => {
 export const logout = async () => {
   let { error } = await supabase.auth.signOut();
   if (error) throw new Error(error.message);
+};
+
+export const updateCurrentUser = async ({ password, fullName, avatar }) => {
+  let updateData;
+
+  if (password) updateData = { password };
+
+  if (fullName) updateData = { data: { fullName } };
+
+  const { data, error } = await supabase.auth.updateUser(updateData);
+
+  if (error) throw new Error(error.message);
+
+  if (!avatar) return data;
+
+  const fileName = `avatar-${data.user.id}-${nanoid()}`;
+  const { error: storageError } = await supabase.storage
+    .from('avatars')
+    .upload(fileName, avatar);
+
+  if (storageError) throw new Error(storageError.message);
+
+  const { data: updatedUser, error: updateUserError } =
+    await supabase.auth.updateUser({
+      data: {
+        avatar: `${supabaseUrl}/storage/v1/object/public/avatars/${fileName}`,
+      },
+    });
+
+  if (updateUserError) throw new Error(updateUserError.message);
+
+  return updatedUser;
 };
