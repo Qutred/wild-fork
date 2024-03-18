caae8e0f4ca48430170a7e195c315a7da0c4d645
Qutred <shloser.andriy@gmail.com>
2024-03-18 17:42:48 +0200

    added header user avatar and menu items

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
 import styled from 'styled-components';
-import Logout from '../features/authentication/Logout';
+import HeaderMenu from './HeaderMenu';
+import UserAvatar from '../features/authentication/UserAvatar';
 
 const StyledHeader = styled.header`
+  display: flex;
+  gap: 2.4rem;
+  align-items: center;
+  justify-content: flex-end;
+
   background-color: var(--color-grey-0);
   padding: 1.2rem 4.8rem;
   border-bottom: 1px solid var(--color-grey-100);
@@ -11,7 +16,8 @@ const StyledHeader = styled.header`
 const Header = () => {
   return (
     <StyledHeader>
-      <Logout />
+      <UserAvatar />
+      <HeaderMenu />
     </StyledHeader>
   );
 };
