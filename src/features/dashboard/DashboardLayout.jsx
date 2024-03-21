6d26c4821255d2560c2cdc3274d977dc5a83fb1d
Qutred <shloser.andriy@gmail.com>
2024-03-21 12:52:09 +0200

    added todays activity  on dashboard

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,6 +6,7 @@ import Stats from './Stats';
 import useCabins from '../cabins/useCabins';
 import SalesChart from './SalesChart';
 import DurationChart from './DurationChart';
+import TodayActivity from '../check-in-out/TodayActivity';
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -18,7 +19,6 @@ const DashBoardlayout = () => {
   const { isLoading: isBookingLoading, bookings } = useRecentBookings();
   const {
     isLoading: isStaysLoading,
-    stays,
     confirmedStays,
     numDays,
   } = useRecentStays();
@@ -34,7 +34,7 @@ const DashBoardlayout = () => {
         numDays={numDays}
         cabinCount={cabins.length}
       />
-      <div>Today&apos;s activity</div>
+      <TodayActivity />
       <DurationChart confirmedStays={confirmedStays} />
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
